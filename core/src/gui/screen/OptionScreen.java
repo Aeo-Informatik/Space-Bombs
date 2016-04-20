66da89adda110f8f90e3ac369a3390461d6d2a0f
sRechner <sRechner@PassisRechner.fritz.box>
2016-04-20 21:54:02 +0200

    Menu

---
+++ core/src/gui/screen/OptionScreen.java
@@ -0,0 +1,58 @@
+package gui.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import gui.screen.Screen;
+import com.badlogic.gdx.graphics.Texture;
+
+public class OptionScreen extends Screen
+{
+private Sprite splash;
+private SpriteBatch batch;
+    @Override
+    public void create() {
+   
+    
+    }
+
+    @Override
+    public void update() {
+        batch = new SpriteBatch();
+        Texture texture = new Texture (Gdx.files.internal("maps/mmenu.png"));  
+        splash = new Sprite(texture);
+        splash.setSize( Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+    }
+ 
+
+    @Override
+    public void render(SpriteBatch sb) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);  
+        batch.begin();
+        splash.draw(batch);
+        batch.end();
+       
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void dispose() {
+   
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+    
+}
+        
