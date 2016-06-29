a4953337c39b1f3ec0d0bb093b5df8433b60c9ff
plonies_d <plonies_d@c045opsi4.EINSTEIN>
2016-06-29 09:17:35 +0200

    Merge origin/master
    Conflicts:
    	core/assets/maps/BasicMap.tmx
    	core/src/com/gdx/bomberman/TODO_first.txt
    	core/src/networkClient/ProcessData.java

--- core/src/com/gdx/bomberman/Constants.java
+++ core/src/com/gdx/bomberman/Constants.java
@@ -29,12 +29,12 @@ public class Constants {
     public static String MAPPATH = "maps/BasicMap.tmx";
     
     //Client config
-    public static String SERVERIP = "";
+    public static String SERVERIP = "localhost";
     public static int CONNECTIONPORT = 13199;
     public static boolean CLIENTDEBUG = false;
     
     //Server config
-    public static boolean TESTSERVER = false; // Only for one player. Starts the game instantly
+    public static boolean TESTSERVER = true; // Only for one player. Starts the game instantly
     public static int SERVERPORT = 13199;
     public static int MAXPLAYERS = 4;
     public static boolean SERVERDEBUG = false;
