8aba2bd8e310345283906f559cb832f4dda66f4c
Luis <infor.aeo@gmail.com>
2017-01-13 17:34:48 +0100

    Build tools optimized for windows

--- core/src/com/gdx/bomberman/Constants.java
+++ core/src/com/gdx/bomberman/Constants.java
@@ -19,8 +19,8 @@ public class Constants
     public static final int CONNECTIONTIMEOUT = 2000; // Milliseconds
 
     // Test settings
-    public static final boolean TESTMAP = true;
-    public static final boolean TINYWINDOWS = true;
+    public static final boolean TESTMAP = false;
+    public static final boolean TINYWINDOWS = false;
     public static final boolean CONTROLLERKEYDEBUG = false;
     public static final String TESTMAPPATH = "maps/Test-Map_(24x24).tmx";
     
